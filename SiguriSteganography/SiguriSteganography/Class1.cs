7b6218aedc9b06d1b69bb557fa017e882d5193f8
EndritMeziu <EndritMeziu@users.noreply.github.com>
2018-05-09 23:01:52 +0200

    Siguria

---
+++ SiguriSteganography/SiguriSteganography/Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SiguriSteganography
+{
+    class Class1
+    {
+        
+        public void main(String[] args)
+        {
+            for(int i=0;i<100;i++)
+            {
+                if ((1 << (i % 8)) > 0)
+                {
+                    Console.WriteLine(i);
+                }
+                else
+                    Console.WriteLine("0");
+            }
+        }
+    }
+}
---
+++ SiguriSteganography/SiguriSteganography/Message.cs
@@ -0,0 +1,48 @@
+﻿
+using System.Collections;
+using System.Text;
+
+namespace steganography
+{
+
+	public class Message
+	{
+
+		 string content; // content of message
+
+		// the constructor
+		public Message(string leMessage)
+		{
+			content = leMessage;
+		}
+
+		
+		public  byte[] toBytes()
+		{
+			byte[] byteTable;
+			
+                
+               byteTable = Encoding.UTF8.GetBytes(content); 
+			
+			return byteTable;
+		}
+
+		
+		public  BitArray parser()
+		{
+			byte[] MessageBytes = this.toBytes();
+			BitArray MessageBits = new BitArray(MessageBytes.Length * 8);
+
+			for (int i = 0; i < MessageBytes.Length * 8; i++)
+			{
+				if ((MessageBytes[MessageBytes.Length - i / 8 - 1] & (1 << (i % 8))) > 0)
+				{
+					MessageBits.Set(i, true);
+				}
+			}
+			return MessageBits;
+		}
+        
+	}
+
+}
